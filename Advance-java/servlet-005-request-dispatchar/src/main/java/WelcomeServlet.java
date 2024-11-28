7eab823438ced0a0d463dbf8384bc37be94459f2
ajaylohar <loharajay0019@gmail.com>
2024-11-28 15:41:12 +0530

    .

---
+++ Advance-java/servlet-005-request-dispatchar/src/main/java/WelcomeServlet.java
@@ -0,0 +1,21 @@
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+
+@WebServlet("/welcome")
+public class WelcomeServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter out =resp.getWriter();
+        out.println("<h1>Welcome</h1>");
+        out.println("<h1>Welcome to Java Servlet</h1>");
+        out.println(LocalDateTime.now());
+    }
+}
---
+++ Advance-java/servlet-005-request-dispatchar/src/main/webapp/index.jsp
@@ -0,0 +1,7 @@
+<html>
+<body>
+<h2>Application Landing Page</h2>
+<a href="login.html"> Login Page </a>
+<a href="search.html"> Search Page </a>
+</body>
+</html>
