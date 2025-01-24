bee030236f047190709ddd18e52ee63558ea5394
ajaylohar <loharajay0019@gmail.com>
2025-01-24 22:10:47 +0530

    .

--- Core-Java/src/PracticeExtraQ/FibonacciSeries.java
+++ Core-Java/src/PracticeExtraQ/FibonacciSeries.java
@@ -1,5 +1,19 @@
 package PracticeExtraQ;
 
 public class FibonacciSeries {
+    public static void main(String[] args) {
+        int n1 = 0, n2 = 1, n3, i, count = 10;
+        System.out.println("Fibonacci Series: ");
+        for (i = 1; i <= count; ++i) {
+            System.out.print(n1 + " ");
+            n3 = n1 + n2;
+            n1 = n2;
+            n2 = n3;
+        }
+
+
+
+
+    }
 
 }
--- Core-Java/src/PracticeExtraQ/RangeOfPrimeNumber.java
+++ Core-Java/src/PracticeExtraQ/RangeOfPrimeNumber.java
@@ -2,17 +2,26 @@ package PracticeExtraQ;
 
 public class RangeOfPrimeNumber {
     public static void main(String[] args) {
-        for (int number =1 ; number<100 ; number++){
+        int count = 0;
+
+        for (int number =1 ; number < 100 ; number++){
             boolean isPrime = true;
+
             for (int i = 2; i <=number/2 ; i++) {
+
                 if (number % i == 0) {
                 isPrime = false;
                  break;
                 }
             }
             if (isPrime) {
-                System.out.println(number);
+
+                System.out.println("\t" +number);
+                count   = count + 1;
             }
         }
+        System.out.println(" number of counts : " + count);
+
+
     }
 }
