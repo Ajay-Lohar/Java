08db06d6926ab038cb51837cc6625b00193d1bb8
ajaylohar <loharajay0019@gmail.com>
2025-02-07 23:15:38 +0530

    .

--- Advance-java/Spring Boot/Project/spring-boot-react-blog-master/blog-frontend/src/pages/index.js
+++
@@ -1,4 +0,0 @@
-export { default as PostPage } from './PostPage';
-export { default as PostListPage } from './PostListPage';
-export { default as EditorPage } from './EditorPage';
-export { default as NotFoundPage } from './NotFoundPage';
