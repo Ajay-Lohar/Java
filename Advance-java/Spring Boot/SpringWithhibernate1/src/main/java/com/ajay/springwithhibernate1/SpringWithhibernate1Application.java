f969f51152d7518ff4dc546c8e4c6f1472fa40a4
ajaylohar <loharajay0019@gmail.com>
2025-02-08 22:48:22 +0530

    .

---
+++ Advance-java/Spring Boot/SpringWithhibernate1/src/main/java/com/ajay/springwithhibernate1/SpringWithhibernate1Application.java
@@ -0,0 +1,13 @@
+package com.ajay.springwithhibernate1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWithhibernate1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWithhibernate1Application.class, args);
+    }
+
+}
---
+++ Advance-java/Spring Boot/SpringWithhibernate1/src/test/java/com/ajay/springwithhibernate1/SpringWithhibernate1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajay.springwithhibernate1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWithhibernate1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
