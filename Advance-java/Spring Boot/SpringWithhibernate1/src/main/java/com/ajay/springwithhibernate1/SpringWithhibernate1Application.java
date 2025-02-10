2e6987a8981b87a303e7b692de34ddebd7a5425b
ajaylohar <loharajay0019@gmail.com>
2025-02-10 22:18:51 +0530

    it not workng some problem in cfg.xml file

--- Advance-java/Spring Boot/SpringWithhibernate1/src/main/java/com/ajay/springwithhibernate1/SpringWithhibernate1Application.java
+++ Advance-java/Spring Boot/SpringWithhibernate1/src/main/java/com/ajay/springwithhibernate1/SpringWithhibernate1Application.java
@@ -1,13 +1,32 @@
 package com.ajay.springwithhibernate1;
 
+import com.ajay.springwithhibernate1.entities.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringWithhibernate1Application {
+class SpringWithHibernate1Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringWithhibernate1Application.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWithHibernate1Application.class, args);
+
+		Configuration cfg = new Configuration();
+		cfg.configure("hibernate.cfg.xml");
+		SessionFactory sessionFactory = cfg.buildSessionFactory();
+		Session session = sessionFactory.openSession();
+
+//		Transaction transaction =session.beginTransaction();
+
+		User user =session.get(User.class, 1L);
+		System.out.println(user.getName());
+		System.out.println(user.getEmail());
+
+
+
+	}
 
 }
